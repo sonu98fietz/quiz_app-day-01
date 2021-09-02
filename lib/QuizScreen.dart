fc9569fc3c512e39e86ca7c74a0c486f2e2e8971
Sonu Kuruvila <84064039+sonu98fietz@users.noreply.github.com>
2021-09-02 10:51:15 +0530

    Add files via upload

---
+++ lib/QuizScreen.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:quiz_app_01/questioncontroller.dart';
+import 'package:quiz_app_01/screens/body.dart';
+
+class QuizScreen extends StatelessWidget {
+  const QuizScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    QuestionController _controller = Get.put(QuestionController());
+
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        actions: [
+          TextButton(
+            onPressed: _controller.nextQuestion,
+            child: const Text("Skip"),
+          )
+        ],
+      ),
+      body: const Body(),
+    );
+  }
+}
